45448da567e540753e05d4a75b0978866c4d637f
yanlee98 <yanlee98@126.com>
2026-02-09 16:03:17 +0800

    同步

--- script2episode.py
+++ script2episode.py
@@ -20,11 +20,11 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # 读取 system.md 文件内容
-with open('script_analysis.md', 'r', encoding='utf-8') as f:
+with open('series_analysis.md', 'r', encoding='utf-8') as f:
     system_content = f.read().strip()
 
 # 读取剧本文件内容
-with open('绝境先知-黄金瞳.txt', 'r', encoding='utf-8') as f:
+with open('script.txt', 'r', encoding='utf-8') as f:
     script_content = f.read().strip()
 
 client = Ark(
